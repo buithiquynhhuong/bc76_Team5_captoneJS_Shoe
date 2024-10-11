5db13fe2adf70b234187e974cb3b59a922c2be66
anhlovegit <ngocanhhuynh95@gmail.com>
2024-10-11 22:33:12 +0700

    add responsive, adjust header style, carousel for details, update product image on frontpage

--- js/details.js
+++ js/details.js
@@ -9,21 +9,50 @@ function changeColor(color) {
 
     if (color === "black") {
         // Change main image
-        mainImage.src = "./images/AIR+MAX+270 black right.png";
+        mainImage.src = "./images/AIR+MAX+270-black-right.png";
 
         // Change thumbnails
-        thumb1.src = "./images/AIR+MAX+270 black left.jpg";
-        thumb2.src = "./images/AIR+MAX+270 behind.png";
-        thumb3.src = "./images/AIR+MAX+270 real.jpg";
-        thumb4.src = "./images/AIR+MAX+270 black top.png";
+        thumb1.src = "./images/AIR+MAX+270black-left.jpg";
+        thumb2.src = "./images/AIR+MAX+270behind.png";
+        thumb3.src = "./images/AIR+MAX+270real.jpg";
+        thumb4.src = "./images/AIR+MAX+270-black-top.png";
     } else if (color === "white") {
         // Change main image
         mainImage.src = "./images/AIR+MAX+270.png";
 
         // Change thumbnails
-        thumb1.src = "./images/AIR+MAX+270 (1).png";
-        thumb2.src = "./images/AIR+MAX+270 (2).png";
-        thumb3.src = "./images/AIR+MAX+270 (3).png";
-        thumb4.src = "./images/AIR+MAX+270 (4).png";
+        thumb1.src = "./images/AIR+MAX+270(1).png";
+        thumb2.src = "./images/AIR+MAX+270(2).png";
+        thumb3.src = "./images/AIR+MAX+270(3).png";
+        thumb4.src = "./images/AIR+MAX+270(4).png";
     }
 }
+
+$(document).ready(function() {
+    $('.carousel-items').slick({
+        slidesToShow: 4, // Default to 4
+        slidesToScroll: 1,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3 // 3 items for medium screens
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2 // 2 items for small screens
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1 // 1 item for extra small screens
+                }
+            }
+        ]
+    });
+});
